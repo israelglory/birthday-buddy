45cde1394fb092af566455ad4f2ce8c4dd2f205e
israelglory <iglory6@gmail.com>
2023-06-12 02:26:55 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let personList = document.getElementById('personList');
+
+let birthdayLenght = document.getElementById('birthdayLenght');
+function removeList() {
+
+    personList.style.display = 'none';
+    birthdayLenght.innerHTML = "0 Birthdays Today"
+}
